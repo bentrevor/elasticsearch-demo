728398c015bd201c8b02cd56c2b00440ef7247aa
Ben Trevor <benjamin.trevor@gmail.com>
2013-05-28 13:25:50 -0500

    Initial commit: Clean application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Searchapp::Application.config.secret_token = 'af94d3d5b95b4653067a13662c5559e83ceb75a4c405bb6d88cc5b4256aff616142809cdc17028c4cf3af09f4d6d8177b1764f07d6b53cdf431fb2372b7f9d60'
